52a4bfb6907ee09a68e4396bbbd0d164d7dfde35
andrzej1naukowiec <andrzej.tenor@gmail.com>
2020-08-03 19:01:12 +0200

    Add new component ValidateDatabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Dashboard } from "./components/pages/Dashboard";
+import { ValidateDatabase } from "./components/pages/ValidateDatabase";
 import "./App.css";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/database" component={ValidateDatabase} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/pages/ValidateDatabase.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function ValidateDatabase() {
+  return <div>Check consistency of database</div>;
+}
